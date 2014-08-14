0812b122008e6f50c97bed229bd68b1dcf4c26b8
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-14 16:53:13 +0530

    onedimensionarray

--- array1d/main.m
+++ array1d/main.m
@@ -12,10 +12,25 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        int i,a[10];
+      
+        for (i=0; i<=10; i++) {
+            NSLog(@"enter the number");
+            scanf("%d",&a[i]);
+        }
+        for (i=0; i<=10; i++) {
+            NSLog(@"the answer is %d",a[i]);
+        }
+//        int i;
+//        int a[10]={7,6,5,3,4,5,6,7,1,2};
+//        for (i=0; i<=10; i++) {
+//           
+//
+//        }
+//        for (i=0; i<=10; i++) {
+//            NSLog(@"the answer is %d",a[i]);
+//        }
+//
     }
     return 0;
 }
